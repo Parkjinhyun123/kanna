5deb18175b0fa4cb1276a7c410d200ee743aebe7
Parkjinhyun123 <143974451+Parkjinhyun123@users.noreply.github.com>
2024-12-02 13:10:56 +0900

    슬라이더 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import styles from "./Modal.module.css";
+import styles from "./Modal.module.css"; // 스타일 파일
 import Left from "../assets/chevron_left_bk.png";
 import Right from "../assets/chevron_right_bk.png";
 import Close from "../assets/close-icon.png";
@@ -8,14 +8,13 @@ import VolumeControl from "./VolumeControl"; // VolumeControl import
 const Modal = ({ isOpen, onClose, images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [volume, setVolume] = useState(0.4); // 초기 볼륨 상태
-  const audioRef = useRef(new Audio("/Sound/사랑의 노래.mp3"));
+  const audioRef = useRef(new Audio("/Sound/사랑의 노래.mp3")); // 오디오 객체 생성
   const [isPlaying, setIsPlaying] = useState(false); // 재생 상태 관리
 
   useEffect(() => {
     if (isOpen) {
       audioRef.current.volume = volume; // 초기 볼륨 설정
 
-      // 재생 요청
       const playAudio = async () => {
         try {
           await audioRef.current.play();
@@ -29,11 +28,10 @@ const Modal = ({ isOpen, onClose, images }) => {
 
       return () => {
         audioRef.current.pause();
-        audioRef.current.currentTime = 0; // 재생 위치 초기화
         setIsPlaying(false); // 재생 상태 업데이트
       };
     }
-  }, [isOpen, volume]); // isOpen 및 volume 상태가 변경될 때마다 실행
+  }, [isOpen]); // isOpen 상태가 변경될 때마다 실행
 
   useEffect(() => {
     // 볼륨이 변경될 때마다 오디오 볼륨 업데이트
